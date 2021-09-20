0acb81aff448f1f61013899c13cc6a4af3200eee
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2021-09-20 23:47:48 +0530

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,14 @@
+//Squareroot calculator
+import java.lang.*;
+class sqCalculator{
+	double i;
+	double x=Math.sqrt(i);
+}
+ class demo2 {
+	public static void main(String[] args) {
+	Calculator a= new sqCalculator();
+	a.i=1000;
+	System.out.println("Square root of " + a.i  + " is "+ a.x);
+}
+
+}
